8ff567b16708496a5814d87340a59608435dfc7a
Nacool <nagariya23nakul@gmail.com>
2020-04-19 22:14:58 +0530

    corrected the paths for deplyment

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,7 @@ weatherForm.addEventListener("submit", (e) => {
   msg1.textContent = "Loding...";
   msg2.textContent = "";
   search.value = "";
-  fetch(`http://localhost:3000/weather?address=${location}`).then((res) => {
+  fetch(`/weather?address=${location}`).then((res) => {
     res.json().then((data) => {
       if (data.error) {
         msg1.textContent = data.error;
